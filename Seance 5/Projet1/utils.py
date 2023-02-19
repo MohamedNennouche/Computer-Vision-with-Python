9433c7652b5d4340601ee7e28cc7bb5d7ddac424
Moh <mohamed.nennouche@g.enp.edu.dz>
2023-02-19 14:27:34 +0100

    Finalisation du projet1 de la séance 5

--- Seance 5/Projet1/utils.py
+++ Seance 5/Projet1/utils.py
@@ -1,10 +1,23 @@
+# Librairies pour la manipulation des fichiers et des variables
 import os
 import glob
 import shutil
 import csv
 
+# Pour la manipulation de matrices
+import numpy as np
+
+# Pour le split des images aléatoirement 
 from sklearn.model_selection import train_test_split
 
+# Pour le prétraitement des images et la mise en place du modèle
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import tensorflow as tf
+
+
+""" 
+============================================== FONCTIONS ================================================
+"""
 
 def split_data(path_to_data:str, path_to_save_train:str, path_to_save_val:str, split_size:float=0.1) -> None:
     """Fonction qui permet de spliter les images d'entrainements aléatoirement en deux dossiers validation et entrainement
@@ -38,8 +51,8 @@ def order_test_set(path_to_images:str, path_to_csv:str) -> None :
     """Fonction permettant de mettre en forme le dossier de test en classes pour permettre l'évaluation du modèle
 
     Args:
-        path_to_images (str): Chemin vers le dossier de test
-        path_to_csv (str): Chemin vers le fichier CSV contenant les classes de chaque image de test
+        - path_to_images (str): Chemin vers le dossier de test
+        - path_to_csv (str): Chemin vers le fichier CSV contenant les classes de chaque image de test
     """
     try : 
         with open(path_to_csv, 'r') as csvfile : # On ouvre le fichier d'une façon temporaire
@@ -59,3 +72,71 @@ def order_test_set(path_to_images:str, path_to_csv:str) -> None :
                 shutil.move(img_full_path, path_to_folder) # on va déplacer et pas copier
     except : 
         print("On ne peut pas ouvrir le fichier csv") 
+
+
+def create_generators(batch_size:int, train_data_path:str, val_data_path:str, test_data_path:str) :
+    """Fonction permettant de mettre en place les générateurs pour les images d'entraînement, de validation et de test
+
+    Args:
+        - batch_size (int): Taille du batch d'images qu'on veut utiliser
+        - train_data_path (str): Chemin vers les images d'entraînements
+        - val_data_path (str): Chemin vers les images de validation
+        - test_data_path (str): Chemin vers les images de test
+
+    Returns:
+        - ImageDataGenerator: Les 3 générateurs d'entrainement, de validation et de test
+    """
+    preprocessor = ImageDataGenerator(
+        rescale = 1/255. # pour assurer une division flottante
+    )
+
+    # !très important pour prendre des données en prenant chaque sous dossier comme classe à part entière
+    train_generator = preprocessor.flow_from_directory(
+        train_data_path,
+        class_mode="categorical",
+        target_size=(60,60), #resize all images
+        color_mode = 'rgb', # type d'images
+        shuffle = True, # tres important
+        batch_size=batch_size
+    )
+
+    val_generator = preprocessor.flow_from_directory(
+        val_data_path,
+        class_mode="categorical",
+        target_size=(60,60), #resize all images
+        color_mode = 'rgb', # type d'images
+        shuffle = False,
+        batch_size=batch_size
+    )
+
+    test_generator = preprocessor.flow_from_directory(
+        test_data_path,
+        class_mode="categorical",
+        target_size=(60,60), #resize all images
+        color_mode = 'rgb', # type d'images
+        shuffle = False,
+        batch_size=batch_size
+    )
+
+    return train_generator, val_generator, test_generator
+
+
+def predict_with_model(model, img_path:str) -> int : 
+    """Fonction pour utiliser notre modèle sur une image
+
+    Args:
+        model (modele Tensorflow): Modèle pré-entrainé chargé en mémoire
+        img_path (str): Chemin vers l'image sur laquelle on veut faire le test
+
+    Returns:
+        int: Prédiction de la classe par la modèle
+    """
+    image = tf.io.read_file(img_path) # On lit l'image
+    image = tf.image.decode_png(image, channels=3) # On la décode
+    image = tf.image.convert_image_dtype(image, dtype=tf.float32) # Convertir les entiers en float, cela permet de rescaler nos images : A VOIR !
+    image = tf.image.resize(image , [60,60]) # resize les images et on a alors de la forme (60,60,3)
+    image = tf.expand_dims(image, axis=0) # pour avoir au final (1,60,60,3) pour l'adapter à notre modèle (voir summary du modèle il attend un tel format)
+
+    prediction = model.predict(image) # peut etre une décision ou un ensemble de probabilités (pour chaque classe)
+    prediction = np.argmax(prediction) # Pour avoir l'indexe de la meilleure probabilité et par conséquent le label
+    return prediction
